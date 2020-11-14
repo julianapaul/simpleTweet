9936960aca8ad3711c93ac34db186ac593b89e9e
Juliana Paul <julianapaul@Julianas-MacBook-Pro.local>
2020-11-13 21:33:06 -0500

    Completed part 2 app requirements

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -3,15 +3,19 @@ package com.codepath.apps.restclienttemplate.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Tweet {
 
     public String body;
     public String createdAt;
     public User user;
+
+    public Tweet(){}
 
     public static Tweet fromJSON(JSONObject jsonObject) throws JSONException {
         Tweet tweet = new Tweet();
@@ -26,7 +30,6 @@ public class Tweet {
         for (int i = 0; i < jsonArray.length(); i++) {
             tweets.add(fromJSON(jsonArray.getJSONObject(i)));
         }
-
         return tweets;
     }
 }
